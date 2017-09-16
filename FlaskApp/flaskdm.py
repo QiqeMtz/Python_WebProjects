f93422fc189f4c9220eaa34ee9a93667f6c18a9b
QiqeMtz <lejm.18@gmail.com>
2017-09-16 00:26:31 -0500

    Full flask project

--- FlaskApp/flaskdm.py
+++ FlaskApp/flaskdm.py
@@ -1,16 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 @app.route('/')
 def index():
-    return 'Hello!'
+    if request.method == 'POST':
+        return 'Esto es un POST'
+    else:
+        return 'Hello!'
 
 @app.route('/hello')
-def hello():
-    return 'Hello, World'
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
 
+@app.route('/usuario/<username>')
 @app.route('/user/<username>')
 def show_user_profile(username):
     # show the user profile for that user
