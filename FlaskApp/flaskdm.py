9918991bc4728399f43848ba0e79c66c321238e8
QiqeMtz <lejm.18@gmail.com>
2017-09-16 00:04:12 -0500

    Modifiec main file

--- FlaskApp/flaskdm.py
+++ FlaskApp/flaskdm.py
@@ -1,18 +1,25 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
-@app.route("/") #decorators
+@app.route('/')
+def index():
+    return 'Hello!'
+
+@app.route('/hello')
 def hello():
-    return "Hello World" 
+    return 'Hello, World'
 
-@app.route("/url2") #decorators
-def new_url():
-    return "Other URLL" 
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
 
-app.route("/post/<int:post_id>", methods=["POST"])
-def mostrar_post(post_id):
-    return "Post %d" % post_id
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
 
 if __name__ == '__main__':
     app.debug = True
