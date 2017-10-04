ff7d60126bd95e2002171c76351a5060bece14b2
QiqeMtz <lejm.18@gmail.com>
2017-10-03 21:07:51 -0500

    [update] Files removed and new directory created

--- OnlineStore/Shoppy/Shoppy/urls.py
+++ OnlineStore/Shoppy/Shoppy/urls.py
@@ -1,4 +1,4 @@
-"""testProject URL Configuration
+"""Shoppy URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
