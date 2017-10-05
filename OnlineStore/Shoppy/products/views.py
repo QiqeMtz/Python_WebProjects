73327446ebfd4715a93c19390c0446f86bd99946
QiqeMtz <lejm.18@gmail.com>
2017-10-04 22:30:14 -0500

    [update] index.html and views.py modified

--- OnlineStore/Shoppy/products/views.py
+++ OnlineStore/Shoppy/products/views.py
@@ -1,9 +1,15 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
 
 def hello_word(request):
     #return HttpResponse('Hello World')
-    #return render(request, 'index.html')
+    #return render(request, 'index.html')+
+    product = Product.objects.order_by('id')
+    template = loader.get_template('index.html')
+    context = {
+        'product': product
+    }
+    return HttpResponse(template.render(context, request))
