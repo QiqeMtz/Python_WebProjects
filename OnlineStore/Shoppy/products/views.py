60d68f7185a6d12f3be77674dae5aabbf4f64c5d
QiqeMtz <lejm.18@gmail.com>
2017-10-04 22:07:53 -0500

    [update] views.py settings.py and urls.py updated, products urls.py and templates folder added

--- OnlineStore/Shoppy/products/views.py
+++ OnlineStore/Shoppy/products/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
+from django.template import loader
 from django.shortcuts import render
 
 # Create your views here.
+
+def hello_word(request):
+    #return HttpResponse('Hello World')
+    #return render(request, 'index.html')
