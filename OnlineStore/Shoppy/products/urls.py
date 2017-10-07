ab30963772c8543ae3a5ea00e6b24e20a6579d3c
QiqeMtz <lejm.18@gmail.com>
2017-10-07 12:40:52 -0500

    [update] Use of Django templates addeds and templates for product details added

--- OnlineStore/Shoppy/products/urls.py
+++ OnlineStore/Shoppy/products/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^$', views.hello_word, name='hello'),
+    # Forma de decir - a partir de mi url root, /producto/ una primary key que contiene sólo números del 0 al 9
+    url(r'^product/(?P<pk>[0-9]+)/$', views.product_detail, name='product_detail'),
 ]
--- OnlineStore/Shoppy/products/views.py
+++ OnlineStore/Shoppy/products/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 # Create your views here.
 
@@ -9,7 +9,20 @@ def hello_word(request):
     #return render(request, 'index.html')+
     product = Product.objects.order_by('id')
     template = loader.get_template('index.html')
+    title = 'Product List'
     context = {
-        'product': product
+        'product': product,
+        'title': title
+    }
+    return HttpResponse(template.render(context, request))
+
+def product_detail(request, pk):
+    # Busca un objeto en la base de datos y su primary key, si no los encuentra devuelve un Error 404
+    product = get_object_or_404(Product, pk=pk)
+    template = loader.get_template('product_detail.html')
+    title = 'Product Details'
+    context = {
+        'product': product,
+        'title': title
     }
     return HttpResponse(template.render(context, request))
