8f8d68e8111e424834ea0c575b164ecbb9c45caa
QiqeMtz <lejm.18@gmail.com>
2017-10-07 01:26:08 -0500

    [update] files modified to allow django work with images using pillow

--- OnlineStore/Shoppy/products/models.py
+++ OnlineStore/Shoppy/products/models.py
@@ -7,6 +7,7 @@ class Product(models.Model):
     description = models.CharField(max_length=255)
     category = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(blank=True)
 
     # Funcion para nombrar al objeto con el valor que se le defina en el modelo
     def __str__(self):
