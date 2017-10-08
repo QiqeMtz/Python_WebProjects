8a799c5843b02e1fa81368d002e60505588d6d49
QiqeMtz <lejm.18@gmail.com>
2017-10-08 16:56:09 -0500

    [update] admin and models updated to add relationship between Clients and Products

--- OnlineStore/Shoppy/products/models.py
+++ OnlineStore/Shoppy/products/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from clients.models import Client
 # Create your models here.
 
 class Product(models.Model):
@@ -16,3 +16,15 @@ class Product(models.Model):
     # Ordenar objetos en la bd, en el admin
     class Meta:
         ordering = ('id',)
+
+#nuevo modelo relacionando productos y clientes utilizando Foreign Keys
+class Favorite(models.Model):
+    user = models.ForeignKey(Client)
+    product = models.ForeignKey(Product)
+
+    class Meta:
+        verbose_name = 'Favorite'
+        verbose_name_plural = 'Favorites'
+
+    def __str__(self):
+        return '%s  %s' % (self.user.name, self.product.name)
