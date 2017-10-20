99f39d0fef7149d8b9932ded4b501314dc71666f
QiqeMtz <lejm.18@gmail.com>
2017-10-19 22:58:01 -0500

    [add] Login module and its functionality added to the application

--- OnlineStore/Shoppy/products/urls.py
+++ OnlineStore/Shoppy/products/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
+    url(r'^login/$', views.auth_login, name='authentication'),
+    url(r'^logout$', auth_views.logout, {'next_page': '/'} , name='logout'),
     url(r'^$', views.ProductList.as_view() , name='hello'),
     # Forma de decir - a partir de mi url root, /producto/ una primary key que contiene sólo números del 0 al 9
     url(r'^product/(?P<pk>[0-9]+)/$', views.ProductDetail.as_view(), name='detail'),
-    url(r'^product/new', views.new_product, name='new_product'),
+    url(r'^product/new', views.new_product, name='new'),
 ]
--- OnlineStore/Shoppy/products/views.py
+++ OnlineStore/Shoppy/products/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
+
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 
 from .models import Product
 from .forms import ProductForm
@@ -47,6 +50,22 @@ def new_product(request):
     }
     return HttpResponse(template.render(context, request))
 
+def auth_login(request):
+    if request.method == 'POST':
+        action = request.POST.get('action', None)
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+
+        if action== 'signup':
+            user = User.objects.create_user(username=username, 
+                                            password=password)
+            user.save()
+        elif action == 'login':
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('/')
+    context = {}
+    return render(request, 'login/login.html', context)
 # using class based views
 class ProductList(ListView):
     model = Product
